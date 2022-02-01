3d48b0e844be3b74dc0b3dd46ffc891e9806ae75
soy001 <97643006+soy001@users.noreply.github.com>
2022-01-31 19:54:38 -0800

    commit new changes
    added to demo and tester files

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,1 +1,16 @@
-
+public class SkillDemo {
+    int x;
+    int y; 
+    
+    public SkillDemo(){
+        this.x = 0;
+        this.y = 0;
+    }
+    public SkillDemo(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    public int add(){
+        return x - y;
+    }
+}
--- SkillDemoTesting.java
+++ SkillDemoTesting.java
@@ -1,1 +1,21 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.*;
 
+public class SkillDemoTesting{
+    private int x;
+    private int y;
+    SkillDemo demo;
+
+    @Before
+    public void setUp(){
+        this.x = 3;
+        this.y = 20;
+        demo = new SkillDemo(x,y);
+    }
+
+    @Test
+    public void testAddition(){
+        assertEquals(23, demo.add());
+    }
+}
